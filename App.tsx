df98298be6062c81356ec9f3d284a0395a4d9880
juam <juanmanso@gmail.com>
2024-08-13 21:08:56 +0200

    chore: init CurrenciesScreen before exercise

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { CurrenciesScreen } from "./src/CurrenciesScreen";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <StatusBar style="auto" />
+        <CurrenciesScreen />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ src/CurrenciesScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export const CurrenciesScreen = () => {
+  return (
+    <View>
+      <Text>Currencies should be listed below</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
