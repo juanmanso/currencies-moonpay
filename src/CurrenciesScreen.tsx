a009da0be65c1ab8842a2a6c048c08f38c76d59c
juam <juanmanso@gmail.com>
2024-08-14 12:36:09 +0200

    feat: use currencies hook fetcher on CurrenciesScreen

--- src/CurrenciesScreen.tsx
+++ src/CurrenciesScreen.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useCurrencies } from "./hooks/useCurrencies";
 
 export const CurrenciesScreen = () => {
+  const { isLoading, isError } = useCurrencies();
+
+  if (isLoading) {
+    return <Text testID="loading">Loading...</Text>;
+  }
+
+  if (isError) {
+    return <Text testID="error">Error fetching data</Text>;
+  }
+
   return (
     <View>
       <Text>Currencies should be listed below</Text>
