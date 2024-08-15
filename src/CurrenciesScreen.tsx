5fcc94cb1122134bf1d7dcae2dda5c9f7f839b1d
juam <juanmanso@gmail.com>
2024-08-15 16:33:01 +0200

    feat: filter by support of testmode
    Initially, I thought about using a Pressable component to toggle states.
    This would have been good in case we need to create our own toggling
    component. However, as said by Iacopo, we didn't need that UI refinement
    so I went to the ReactNative docs to pull the sample code for the Switch
    component and use that instead. A few minor modifications were needed
    since code was different but it was basically it.
    
    Wanted to make a row view to put the text alongside it but during the
    exercise it was expressed the lack of need of that so it was left as a
    column container which doesn't make much sense visually but works for
    the test.

--- src/CurrenciesScreen.tsx
+++ src/CurrenciesScreen.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Switch, Text, View } from "react-native";
 import { CurrenciesListHeader, CurrenciesListItem } from "./components";
 import { useCurrencies } from "./hooks/useCurrencies";
+import { Currencies } from "./types";
 
 export const CurrenciesScreen = () => {
   const { data, isLoading, isError } = useCurrencies();
+  const [filteredData, setFilteredData] = useState<Currencies>([]);
+  const [showTestMode, setShowTestMode] = useState(false);
+
+  useEffect(() => {
+    if (showTestMode) {
+      setFilteredData(data.filter((item) => item.supportsTestMode));
+    } else {
+      setFilteredData(data);
+    }
+  }, [data, showTestMode]);
 
   if (isLoading) {
     return <Text testID="loading">Loading...</Text>;
@@ -16,9 +27,18 @@ export const CurrenciesScreen = () => {
 
   return (
     <View testID="container">
-      <Text>Currencies should be listed below</Text>
+      <View>
+        <Text>Toggle test mode filtering</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={showTestMode ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={setShowTestMode}
+          value={showTestMode}
+        />
+      </View>
       <FlatList
-        data={data}
+        data={filteredData}
         renderItem={({ item }) => <CurrenciesListItem item={item} />}
         keyExtractor={(item) => item.code}
         ListHeaderComponent={<CurrenciesListHeader />}
